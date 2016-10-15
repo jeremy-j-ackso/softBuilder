18df5ee25138a4c23ea95f17400ca823132e5fbd
jeremy-j-ackso <jeremy_j@ackso.net>
2016-10-15 16:24:31 -0600

    Added more tutorials.

---
+++ exec10.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+#! -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTextEdit
+from PyQt5.QtGui import QIcon
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QAction(QIcon('exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit Application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+
+        self.setGeometry(300,300,350,250)
+        self.setWindowTitle('Main Window')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
--- exec4.py
+++ exec4.py
@@ -2,8 +2,8 @@
 #! -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
 
 class Example(QWidget):
     def __init__(self):
@@ -11,14 +11,12 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        QToolTip.setFont(QFont('SansSerif', 10))
-        self.setToolTip('This is a <b>QWidget</b> widget')
-        btn = QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushbutton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.move(50,50)
-        self.setGeometry(300,300,300,200)
-        self.setWindowTitle('Tooltips')
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50,50)
+        self.setGeometry(300,300,250,150)
+        self.setWindowTitle('Quit Button')
         self.show()
 
 if __name__ == '__main__':
